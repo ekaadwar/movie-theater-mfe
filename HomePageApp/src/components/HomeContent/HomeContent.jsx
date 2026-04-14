f040c8d1b625d27f1e3ff0b9decbaf6e59377296
Eka Fajhari Adwar <eka.adwar@gmail.com>
2026-04-14 16:35:37 +0700

    fetching movies data from MF_Server using fetch method

--- HomePageApp/src/components/HomeContent/HomeContent.jsx
+++ HomePageApp/src/components/HomeContent/HomeContent.jsx
@@ -7,8 +7,10 @@ const dummyItem = [{name:"Dummy Movie"}]
 const HomeContent = (props) => {
   const [movies, setMovies] = useState(dummyItem);
 
-  useEffect(() => {
-    // Add the logic to load the movies from server and set to the state
+  useEffect(async () => {
+    const resp = await fetch("http://localhost:5555/movies")
+    const data = await resp.json()
+    console.log({data})
   }, []);
 
   const movieClicked = (item) => {
